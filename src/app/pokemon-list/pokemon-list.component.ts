2801cecacb20b52be838fcc6e6fea1e719cdcacf
Mateus Bentes <mateus1337bentes@gmail.com>
2021-04-17 00:33:16 -0400

    comment unused varible

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -9,35 +9,35 @@ import { PokemonService } from '../_services/pokemon.service';
   styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent {
-  public pokemons: Pokemon[] = [
-    {
-      number: '1',
-      image:
-        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
-      name: 'Bulbasaur',
-      types: [Type.Grass, Type.Poison],
-    },
-    {
-      number: '2',
-      image:
-        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
-      name: 'Ivysaur',
-      types: [Type.Grass, Type.Poison],
-    },
-    {
-      number: '1',
-      image:
-        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
-      name: 'Bulbasaur',
-      types: [Type.Grass, Type.Poison],
-    },
-    {
-      number: '4',
-      image:
-        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
-      name: 'Charmander',
-      types: [Type.Fire],
-    },
-  ];
+  // public pokemons: Pokemon[] = [
+  //   {
+  //     number: '1',
+  //     image:
+  //       'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+  //     name: 'Bulbasaur',
+  //     types: [Type.Grass, Type.Poison],
+  //   },
+  //   {
+  //     number: '2',
+  //     image:
+  //       'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+  //     name: 'Ivysaur',
+  //     types: [Type.Grass, Type.Poison],
+  //   },
+  //   {
+  //     number: '1',
+  //     image:
+  //       'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+  //     name: 'Bulbasaur',
+  //     types: [Type.Grass, Type.Poison],
+  //   },
+  //   {
+  //     number: '4',
+  //     image:
+  //       'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
+  //     name: 'Charmander',
+  //     types: [Type.Fire],
+  //   },
+  // ];
   constructor(public pokemonService: PokemonService) {}
 }
